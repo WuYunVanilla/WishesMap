7caad4ea46550cd783ae6362a9a2611da11b2ba4
wy <15302010050@fudan.edu.cn>
2018-05-06 16:09:25 +0800

    final

--- users.sql
+++ users.sql
@@ -1,7 +1,16 @@
 
+DROP TABLE IF EXISTS `users`;
+
 CREATE TABLE `users`(
   user_ID INT  NOT NULL AUTO_INCREMENT PRIMARY KEY ,
   user_name text ,
   user_pass text
  )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
+INSERT INTO `users` (user_ID, user_name, user_pass) VALUE (0, 'admin', '123456');
+INSERT INTO `users` (user_ID, user_name, user_pass) VALUE (0, 'root', '123456');
+INSERT INTO `users` (user_ID, user_name, user_pass) VALUE (0, 'tomcat', '123456');
+INSERT INTO `users` (user_ID, user_name, user_pass) VALUE (0, 'servlet', '123456');
+
+SELECT count(*) AS `count`, user_ID FROM users WHERE user_name = 'admin' AND user_pass = '12356';
