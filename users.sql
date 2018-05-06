2b55c53e9b087da7caeeaf765e3b81120bd7e993
wy <15302010050@fudan.edu.cn>
2018-05-06 16:13:11 +0800

    final!!!

--- users.sql
+++ users.sql
@@ -8,9 +8,9 @@ CREATE TABLE `users`(
  )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-INSERT INTO `users` (user_ID, user_name, user_pass) VALUE (0, 'admin', '123456');
-INSERT INTO `users` (user_ID, user_name, user_pass) VALUE (0, 'root', '123456');
-INSERT INTO `users` (user_ID, user_name, user_pass) VALUE (0, 'tomcat', '123456');
-INSERT INTO `users` (user_ID, user_name, user_pass) VALUE (0, 'servlet', '123456');
+INSERT INTO `users` (user_ID, user_name, user_pass) VALUE (1, 'admin', '123456');
+INSERT INTO `users` (user_ID, user_name, user_pass) VALUE (2, 'root', '123456');
+INSERT INTO `users` (user_ID, user_name, user_pass) VALUE (3, 'tomcat', '123456');
+INSERT INTO `users` (user_ID, user_name, user_pass) VALUE (4, 'servlet', '123456');
 
 SELECT  user_ID FROM users WHERE user_name = 'root' AND user_pass = '123456';
