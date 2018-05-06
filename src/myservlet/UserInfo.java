1b5bea674cbbd8cd9e731170771ac07f74bb02d8
wy <15302010050@fudan.edu.cn>
2018-05-06 22:24:46 +0800

    build war

---
+++ src/myservlet/UserInfo.java
@@ -0,0 +1,12 @@
+package myservlet;
+
+public class UserInfo {
+    public final String user_name;
+    public final String user_pass;
+    public UserInfo(String user_name, String user_pass) {
+        this.user_name = user_name;
+        this.user_pass = user_pass;
+    }
+
+
+}
